5f648642265cd307f5cdd696ff4a01ec708f7e6e
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-10-10 11:40:41 +0300

    Initial commit

---
+++ src/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+    private int edge;
+
+
+    public int calculateArea() {
+        return edge*edge;
+    }
+
+    public int calculatePrimeter() {
+        return edge*4;
+    }
+}
